e11d63630c2343add71cd20100cfa73b97b0fb56
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-04-11 19:39:33 -0700

    tweets are working needs styles and setting time correctly

--- app.js
+++ app.js
@@ -13,16 +13,48 @@ $(document).ready(function(){
    var $username = $('<a class="username" data-user="' + tweet.user + '"></a>');
    $username.text('@' + tweet.user);
 
-
+   // time of tweet
+   var date = tweet.created_at;
+   var mDate = moment().startOf('hour').fromNow();
+   console.log(mDate);
+   var $creationTime = $('<span class="creation-time">' + date + '</span>');
+   $tweet.text(' ' + tweet.message + ' ');
+   $username.prependTo($tweet);
+   $creationTime.appendTo($tweet);
+   // $creationTime.startOf('hour').fromNow();
+   return $tweet;
  };
 
- var index = streams.home.length - 1;
- while(index >= 0){
-   var tweet = streams.home[index];
-   var $tweet = $('<div></div>');
-   $tweet.text('@' + tweet.user + ': ' + tweet.message);
-   $tweet.appendTo($body);
-   index -= 1;
- }
+ var lastNewest = streams.home.length - 1;
+
+ // var index = streams.home.length - 1;
+
+ // add latest tweets
+ var addNewTweets = function(newest, oldest, collection) {
+   if (collection === undefined) {
+    collection = streams.home;
+   }
+   while (oldest <= newest) {
+    buildTweet(collection[oldest]).prependTo($tweets);
+    oldest++;
+   }
+   
+   latest = newest;
+ };
+
+ addNewTweets(streams.home.length - 1, 0);
+
+ $($button).on('click', function(){
+   addNewTweets(streams.home.length - 1, lastNewest + 1);
+ });
+
+
+ // while(index >= 0){
+ //   var tweet = streams.home[index];
+ //   var $tweet = $('<div></div>');
+ //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
+ //   $tweet.appendTo($body);
+ //   index -= 1;
+ // }
 
 });
